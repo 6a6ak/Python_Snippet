1a3babd9c956b87bc2f50eca5fdf8fbeb7adf295
Babak Ahari <babak.ahari@gmail.com>
2022-09-18 16:30:44 +0200

    Update T-T2-T3plots.py
    change -- to *

--- T-T2-T3plots.py
+++ T-T2-T3plots.py
@@ -3,6 +3,6 @@ import matplotlib.pyplot as plt
 t=np.arange (0,5,0.1)
 plt.plot ([1,2,3,4],[1,2,3,4], 'or')
 plt.plot (t,t**2,'g^')
-plt.plot (t,t**3,'b--')
+plt.plot (t,t**3,'b*')
 plt.ylabel('Y');plt.xlabel('X')
 plt.show()
